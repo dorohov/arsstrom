c27613d6c43b26d70e65d9679a5e7eea2a1030ac
Pavel Proyavin <pavel@dorohovdesign.com>
2019-06-04 17:53:36 +0300

    changelog from 040619

---
+++ src/js/banner.js
@@ -0,0 +1,14 @@
+(function($) {
+    "use strict"
+    $(function() {
+
+        $('.in-banner__items').slick({
+            prevArrow: '.in-banner__arrows button.is--prev',
+            nextArrow: '.in-banner__arrows button.is--next',
+            fade: true,
+            cssEase: 'linear',
+            draggable: false
+        })
+
+    })
+})(jQuery);
---
+++ src/js/navbar.js
@@ -0,0 +1,82 @@
+(function($) {
+    "use strict"
+    $(function() {
+
+        var searchField = $('.navbar__userpanel__field')
+
+        function openOverlay() {
+            $('#overlay').show();
+            $('body').addClass('is--overlay')
+        }
+        
+        function closeOverlay() {
+            $('#overlay').hide();
+            $('body').removeClass('is--overlay')
+        }
+
+        function openSearchField() {
+            $('.navbar__userpanel').addClass('is--open')
+            searchField
+                .children('form')
+                .children('input[type="text"]')
+                .focus()
+        }
+
+        function closeSearchField() {
+            $('.navbar__userpanel').removeClass('is--open') 
+        }
+
+        function openMobileMenu() {
+            $('.navbar__btn button').addClass('is-active')
+            $('body').addClass('is--mm')
+            openOverlay()
+        }
+
+        function closeMobileMenu() {
+            $('.navbar__btn button').removeClass('is-active')
+            $('body').removeClass('is--mm')
+            closeOverlay()
+        }
+
+        function setBodyPadding() {
+            var padding = $('.navbar').innerHeight();
+            $('body').css({
+                paddingTop: padding + 'px'
+            })
+        }
+
+        $('#overlay').on('click', function() {
+            closeOverlay()
+            closeMobileMenu()
+        })
+
+        setBodyPadding()
+
+        $('#openSearchNavbarField').on('click', function() {
+            if(!searchField.hasClass('is--open')) {
+                openSearchField()
+            }else {
+                closeSearchField()
+            }
+        })
+
+        $(window).resize(function() {
+            setBodyPadding()
+        })
+
+        $(document).on('click', function(e) {
+            if($(e.target).closest('.navbar__userpanel').length <= 0) {
+                closeSearchField()
+            }
+        })
+
+        $('.navbar__btn button').on('click', function() {
+            if(!$('.navbar__btn button').hasClass('is-active')) {
+                openMobileMenu() 
+            }else {
+                closeMobileMenu()
+            }
+        })
+
+    })
+})(jQuery);
